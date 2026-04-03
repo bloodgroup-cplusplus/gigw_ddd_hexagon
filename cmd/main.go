9d6f99a30c334af37da81b8d9f3be4279db0a37f
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2026-04-03 11:45:24 +0530

    adding port for the application layer

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
 
+	"github.com/bloodgroup-cplusplus/gigw_ddd_hexagon/internal/adapters/core/arithmetic"
+	//"github.com/bloodgroup-cplusplus/gigw_ddd_hexagon/internal/ports"
+)
 
 func main() {
+	// ports
+
+	//var core ports.ArithmeticPort
+
+	//core = arithmetic.NewAdapter()
+	arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 3)
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	fmt.Println(result)
 
 }
