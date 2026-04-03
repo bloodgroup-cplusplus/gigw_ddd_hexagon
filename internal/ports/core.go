1a0858e7212912a616174ca948bb6a6aef3f26d1
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2026-04-03 10:38:15 +0530

    adding ports

--- internal/ports/core.go
+++ internal/ports/core.go
@@ -1,7 +1,8 @@
 package ports
-import (
-	"fmt"
-)
-func main() {
-	fmt.Pritnln("vim-go")
+
+type ArithmeticPort interface {
+	Addition(a int32, b int32) (int32, error)
+	Subtraction(a int32, b int32) (int32, error)
+	Multiplication(a int32, b int32) (int32, error)
+	Divison(a int32, b int32) (int32, error)
 }
